42b5104924d85ba94759591119a8aabb228edaf8
sofyanaarrass <sofyan_ilias@hotmail.com>
2022-07-11 09:59:32 +0200

    version 2

--- perturbed.jl
+++ perturbed.jl
@@ -21,29 +21,36 @@ function random_distance_vector(d, n)
 end
 
 ### Game of Chicken, Swerve VS Stay
-game9 = generate_game(
+game = generate_game(
         [0 0; 1 -1],
         [-1 1; -20 -20]
         );
 
-# Having a perfect 1 or 0 chance results in a Inaccuracy error
-s = [[.99, .01], [0.01, .99]] # Player1 Swerves and Player2 Stays
-# low distances "never" diverge while larger distances diverge quite quickly as there are 2 NE's
+for distance in 0:0.1:1
+    # Having a perfect 1 or 0 chance results in a Inaccuracy error
+    s = [[.99, .01], [0.01, .99]] # Player1 Swerves and Player2 Stays
+    # low distances "never" diverge while larger distances diverge quite quickly as there are 2 NE's
 
-previous_ne = last(iterate_best_reply(game, s))
-new_ne = previous_ne;
-c = 0
-while(new_ne == previous_ne && c < MAX_LOOPS)
-    global c, new_ne; # global as Julia sees loops the same as functions and will create separate, local variables.
-    s[1] = perturb(s[1], random_distance_vector(DISTANCE, 2));# disturb both players by a random vector
-    s[2] = perturb(s[2], random_distance_vector(DISTANCE, 2));
-    new_ne = last(iterate_best_reply(game, s));
-    
-    c += 1;
-end
+    oldstd = stdout
+    redirect_stdout(open("/dev/null", "w"))
 
-if(c == MAX_LOOPS)
-    println("Does not diverge(within specified limit)")
-else
-    println("Diverged after " * string(c) * " cycles")
+    previous_ne = last(iterate_best_reply(game, s));
+    new_ne = previous_ne;
+    c = 0
+    while(new_ne == previous_ne && c < MAX_LOOPS)
+        #global c, new_ne; # global as Julia sees loops the same as functions and will create separate, local variables.
+        s[1] = perturb(s[1], random_distance_vector(distance, 2));# disturb both players by a random vector
+        s[2] = perturb(s[2], random_distance_vector(distance, 2));
+        new_ne = last(iterate_best_reply(game, s));
+        
+        c += 1;
+    end
+
+    redirect_stdout(oldstd) # recover original stdout
+
+    if(c == MAX_LOOPS)
+        println("Does not diverge(within specified limit) for distance " * string(distance))
+    else
+        println("Diverged after " * string(c) * " cycles for distance " * string(distance))
+    end
 end
